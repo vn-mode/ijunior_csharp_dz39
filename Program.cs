b61eeeca4c90e5b073745113c4904cc3e61bbee6
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-02-25 18:08:23 +0300

    ijunior_csharp_dz39

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+
+namespace vn_mode_csharp_dz_39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player();
+            Renderer renderer = new Renderer();
+            renderer.GetPlayerCoordinates();
+        }
+
+        class Player
+        {
+            protected int _x = 10;
+            protected int _y = 10;
+        }
+
+        class Renderer : Player
+        {
+            private char Player = '@';
+
+            public void GetPlayerCoordinates()
+            {
+                Console.SetCursorPosition(_x, _y);
+                Console.Write(Player);
+            }
+        }
+    }
+}
+
